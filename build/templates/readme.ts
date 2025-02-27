24930529f777202845571462d96260a103fcef1a
mistic100 <contact@git.strangeplanet.fr>
2025-02-27 22:09:31 +0100

    doc: badge links to npm trends

--- build/templates/readme.ts
+++ build/templates/readme.ts
@@ -2,7 +2,7 @@ export const readme = (pkg: any) =>
     `# ${pkg.psv.title}
 
 [![NPM version](https://img.shields.io/npm/v/${pkg.name}?logo=npm)](https://www.npmjs.com/package/${pkg.name})
-[![NPM Downloads](https://img.shields.io/npm/dm/${pkg.name}?color=f86036&label=npm&logo=npm)](https://www.npmjs.com/package/${pkg.name})
+[![NPM Downloads](https://img.shields.io/npm/dm/${pkg.name}?color=f86036&label=npm&logo=npm)](https://npmtrends.com/${pkg.name})
 [![jsDelivr Hits](https://img.shields.io/jsdelivr/npm/hm/${pkg.name}?color=%23f86036&logo=jsdelivr)](https://www.jsdelivr.com/package/npm/${pkg.name})
 
 ${pkg.description}
